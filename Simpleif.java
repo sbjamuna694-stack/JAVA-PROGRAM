cf3cc5ee827229faf6fa49fd656f8309ed0a90bc
sbjamuna694-stack <sbjamuna694@gmail.com>
2026-09-04 21:40:30 +0530

    Add files via upload

---
+++ Simpleif.java
@@ -0,0 +1,9 @@
+public class Simpleif {
+    public static void main(String[] args) {
+        int a = 18;
+
+        if (a >=18) {
+            System.out.println("your eligible to vote");
+        }
+    }
+}
