128d5831f161d0362ff30108b067918a2a1602b5
sbjamuna694-stack <sbjamuna694@gmail.com>
2026-09-04 21:40:53 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,13 @@
+class ifelse {
+    public static void main(String[] args) {
+        int age =20;
+        if(age>=20){
+            System.out.println("your eligible to vote");
+        }else{
+            System.out.println("your not eligible to vote");
+
+
+        }
+        }
+    }
+
