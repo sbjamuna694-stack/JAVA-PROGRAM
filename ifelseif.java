4eff065fbfdcfe6ac9457e4ab7dc98569ea3908a
sbjamuna694-stack <sbjamuna694@gmail.com>
2026-09-04 21:41:12 +0530

    Add files via upload

---
+++ ifelseif.java
@@ -0,0 +1,15 @@
+class ifelseif {
+    public static void main(String[] args){
+        int income = 30000;
+          if(income>1000000){
+             System.out.println("he is rich");
+          }
+          else if(income<1000000 && income>50000){
+            System.out.println("he is middle class");
+          }
+          else{
+            System.out.println("he is poor");
+          }
+
+    }
+}
