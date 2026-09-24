b2ec49d71b30a83a9bb400695e86e5d531a6ef59
JAMUNA SB <sbjamuna694@gmail.com>
2026-09-24 08:07:57 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,10 @@
+class main{
+    public static int square (int num){
+return num*num;
+}
+public static void main(String[] args){
+    int result;
+    result=square(10);
+    System.out.println("square of 10 is: "+result);
+}
+}
